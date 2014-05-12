fbc7c4fe7bee5dded849eea9e9c8545f161975a8
meh <meh@schizofreni.co>
2014-05-12 11:11:07 +0200

    mix: fix deps

--- mix.exs
+++ mix.exs
@@ -2,9 +2,16 @@ defmodule CommonSense.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :common_sense,
-     version: "0.0.1",
-     elixir: "~> 0.13.3-dev",
-     deps: deps]
+    [ app: :common_sense,
+      version: "0.0.1",
+      elixir: "~> 0.13.3-dev",
+      package: package,
+      description: "Common sense for Elixir" ]
+  end
+
+  defp package do
+    [ contributors: ["meh"],
+      license: "WTFPL",
+      links: [ { "GitHub", "https://github.com/meh/elixir-common-sense" } ] ]
   end
 end
